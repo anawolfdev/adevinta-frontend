92dd73f6a4456f21ff634cbb972ded740ce4d958
Ana Amaro <amarovazquezana@gmail.com>
2020-01-12 15:28:55 +0100

    Added some styles, functions in js & update readme.md/package.json

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,20 @@
+/* eslint-disable indent */
 'use strict';
 
-console.log('>> Ready :)');
+const collapsibleTrigger = document.querySelectorAll('.js-collapsible-trigger');
+
+function updateCollapsible(event) {
+	const currentCollapsible = event.currentTarget.parentElement;
+	if (currentCollapsible.classList.contains('collapsible-open')) {
+		currentCollapsible.classList.remove('collapsible-open');
+	} else {
+		for (const item of collapsibleTrigger) {
+			item.parentElement.classList.remove('collapsible-open');
+		}
+		currentCollapsible.classList.add('collapsible-open');
+	}
+}
+
+for (const item of collapsibleTrigger) {
+	item.addEventListener('click', updateCollapsible);
+}
