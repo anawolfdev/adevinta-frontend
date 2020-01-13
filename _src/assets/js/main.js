9f979af342967c984e7bd4ef99b9a38d26d8d3f3
Ana Amaro <amarovazquezana@gmail.com>
2020-01-13 13:09:43 +0100

    fixed main.js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,16 +1,16 @@
 /* eslint-disable indent */
 'use strict';
 
-const collapsibleTrigger = document.querySelectorAll('.js-collapsible-trigger');
 const ajaxSectionFour = document.querySelector('.ajax-section');
 const ajaxContent = document.querySelector('.ajax-content');
+const collapsibleSection = document.querySelectorAll('.js-collapsible-section');
 
 function updateCollapsible(event) {
 	const currentCollapsible = event.currentTarget.parentElement;
 	if (currentCollapsible.classList.contains('collapsible-open')) {
 		currentCollapsible.classList.remove('collapsible-open');
 	} else {
-		for (const item of collapsibleTrigger) {
+		for (const item of collapsibleSection) {
 			item.parentElement.classList.remove('collapsible-open');
 		}
 		currentCollapsible.classList.add('collapsible-open');
@@ -27,6 +27,6 @@ function ajaxSection() {
 
 ajaxSectionFour.addEventListener('click', ajaxSection);
 
-for (const item of collapsibleTrigger) {
+for (const item of collapsibleSection) {
 	item.addEventListener('click', updateCollapsible);
 }
